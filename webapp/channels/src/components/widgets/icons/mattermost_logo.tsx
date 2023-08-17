1464feb4c7d62fede5d7c9b59f7b97d9ddf0d5e3
Stefan Akie <stefan.akie@grommunio.com>
2023-08-17 07:45:23 -0400

    Exchange system message icon

--- webapp/channels/src/components/widgets/icons/mattermost_logo.tsx
+++ webapp/channels/src/components/widgets/icons/mattermost_logo.tsx
@@ -12,23 +12,56 @@ export default function MattermostLogo(props: React.HTMLAttributes<HTMLSpanEleme
                 version='1.1'
                 x='0px'
                 y='0px'
-                viewBox='0 0 500 500'
+                viewBox='0 0 114.16 100.51'
                 enableBackground='new 0 0 500 500'
                 role='img'
                 aria-label={formatMessage({id: 'generic_icons.mattermost', defaultMessage: 'grommunio-chat Logo'})}
             >
-                <g>
-                    <g>
-                        <path
-                            style={style}
-                            d='M396.9,47.7l2.6,53.1c43,47.5,60,114.8,38.6,178.1c-32,94.4-137.4,144.1-235.4,110.9 S51.1,253.1,83,158.7C104.5,95.2,159.2,52,222.5,40.5l34.2-40.4C150-2.8,49.3,63.4,13.3,169.9C-31,300.6,39.1,442.5,169.9,486.7 s272.6-25.8,316.9-156.6C522.7,223.9,483.1,110.3,396.9,47.7z'
-                        />
-                    </g>
-                    <path
-                        style={style}
-                        d='M335.6,204.3l-1.8-74.2l-1.5-42.7l-1-37c0,0,0.2-17.8-0.4-22c-0.1-0.9-0.4-1.6-0.7-2.2 c0-0.1-0.1-0.2-0.1-0.3c0-0.1-0.1-0.2-0.1-0.2c-0.7-1.2-1.8-2.1-3.1-2.6c-1.4-0.5-2.9-0.4-4.2,0.2c0,0-0.1,0-0.1,0 c-0.2,0.1-0.3,0.1-0.4,0.2c-0.6,0.3-1.2,0.7-1.8,1.3c-3,3-13.7,17.2-13.7,17.2l-23.2,28.8l-27.1,33l-46.5,57.8 c0,0-21.3,26.6-16.6,59.4s29.1,48.7,48,55.1c18.9,6.4,48,8.5,71.6-14.7C336.4,238.4,335.6,204.3,335.6,204.3z'
-                    />
-                </g>
+                <path
+                    style={grommunioStyle.st0}
+                    d='M114.16,5.52,82.32,19.91,94.53,40.17l19.63-34Z'
+                />
+                <path
+                    style={grommunioStyle.st0}
+                    d='M31,23.6,33.08,0H1Z'
+                />
+                <path
+                    style={grommunioStyle.st1}
+                    d='M31.64,52.25l18.75,2.1L35.62,37.73Z'
+                />
+                <path
+                    style={grommunioStyle.st1}
+                    d='M29.86,31.79,0,8.3,24.6,50.92Z'
+                />
+                <path
+                    style={grommunioStyle.st1}
+                    d='M37.93,25.63,73,15.79,69.61,0H40.26Z'
+                />
+                <path
+                    style={grommunioStyle.st1}
+                    d='M71.48,23.62,40.37,32.34,60.19,54.65Z'
+                />
+                <path
+                    style={grommunioStyle.st1}
+                    d='M79.75,13.25,109.08,0H76.9Z'
+                />
+                <path
+                    style={grommunioStyle.st1}
+                    d='M68.18,53.55l21-4.18,1.28-2.12L78,26.54Z'
+                />
+                <path
+                    style={grommunioStyle.st0}
+                    d='M55.5,62.11l-26.18-3c11.8,20.43,21.51,37.26,23.91,41.4Z'
+                />
+                <path
+                    style={grommunioStyle.st1}
+                    d='M62.51,64.25l-2.07,35,13.22-22.9Z'
+                />
+                <path
+                    style={grommunioStyle.st1}
+                    d='M68.88,60.68l8.51,9.18,7.09-12.28Z'
+                />
+
             </svg>
         </span>
     );
@@ -37,4 +70,13 @@ export default function MattermostLogo(props: React.HTMLAttributes<HTMLSpanEleme
 const style: CSSProperties = {
     fillRule: 'evenodd',
     clipRule: 'evenodd',
+};
+
+const grommunioStyle: any = {
+    st0: {
+        fill: '#3f3f3f',
+    },
+    st1: {
+        fill: '#2dcaff',
+    },
 };
