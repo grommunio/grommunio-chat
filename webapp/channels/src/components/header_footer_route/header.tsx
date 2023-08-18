6dc365efacb70796d8ef941d8b23cee0f6cec013
Stefan Akie <stefan.akie@grommunio.com>
2023-08-18 04:24:02 -0400

    Exchange header logo

--- webapp/channels/src/components/header_footer_route/header.tsx
+++ webapp/channels/src/components/header_footer_route/header.tsx
@@ -7,6 +7,7 @@ import {useSelector} from 'react-redux';
 
 import BackButton from 'components/common/back_button';
 import Logo from 'components/common/svg_images_components/logo_dark_blue_svg';
+import logo from 'images/logo.svg';
 
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
 
@@ -31,7 +32,11 @@ const Header = ({alternateLink, backButtonURL, onBackButtonClick}: HeaderProps) 
                     to='/'
                     aria-label={ariaLabel}
                 >
-                    {EnableCustomBrand === 'true' || SiteName !== 'grommunio-chat' ? SiteName : <Logo/>}
+                    {EnableCustomBrand === 'true' || SiteName !== 'grommunio-chat' ? SiteName : <img
+                        src={logo}
+                        width={169}
+                        className='get-app__logo'
+                    />}
                 </Link>
                 {alternateLink}
             </div>
