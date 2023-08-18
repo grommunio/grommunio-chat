73155c766e6b932ed8e7441c3910a702232863bb
Victor Tesla <victor.tesla@grommunio.com>
2023-08-18 14:25:02 +0200

    config: enable local unix socket at a different location

--- server/public/model/config.go
+++ server/public/model/config.go
@@ -250,7 +250,7 @@ const (
 
 	OpenidSettingsDefaultScope = "profile openid email"
 
-	LocalModeSocketPath = "/var/tmp/mattermost_local.socket"
+	LocalModeSocketPath = "/var/tmp/grommunio-chat_local.socket"
 )
 
 func GetDefaultAppCustomURLSchemes() []string {
@@ -829,7 +829,7 @@ func (s *ServiceSettings) SetDefaults(isUpdate bool) {
 	}
 
 	if s.EnableLocalMode == nil {
-		s.EnableLocalMode = NewBool(false)
+		s.EnableLocalMode = NewBool(true)
 	}
 
 	if s.LocalModeSocketLocation == nil {
