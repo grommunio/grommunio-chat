1a53f868f3c8b20b21939407267c6b5e3f108861
Moritz Lederle <moritz.lederle@grommunio.com>
2024-10-15 08:00:20 +0200

    Removed menu start-trial text

--- webapp/channels/src/components/widgets/menu/menu_items/menu_start_trial.tsx
+++ webapp/channels/src/components/widgets/menu/menu_items/menu_start_trial.tsx
@@ -45,30 +45,7 @@ const MenuStartTrial = (props: Props): JSX.Element | null => {
     }
 
     return (
-        <li
-            className={'MenuStartTrial'}
-            role='menuitem'
-            id={props.id}
-        >
-            <div className='editionText'>
-                {formatMessage(
-                    {
-                        id: 'navbar_dropdown.versionText',
-                        defaultMessage: 'This is the free <link>unsupported</link> edition of Mattermost.',
-                    },
-                    {
-                        link: (msg: React.ReactNode) => (
-                            <ExternalLink
-                                location='menu_start_trial.unsupported-link'
-                                href={LicenseLinks.UNSUPPORTED}
-                            >
-                                {msg}
-                            </ExternalLink>
-                        ),
-                    },
-                )}
-            </div>
-        </li>
+       <></>
     );
 };
 
